ae9c916655e3982d4f4398e6f8a81f78a74cca5b
Minho Kim <xtabletx@gmail.com>
2020-06-08 07:55:31 -0500

    https://ssungkang.tistory.com/

--- hcsku/settings.py
+++ hcsku/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import dj_database_url
 import os
 import json
 from django.core.exceptions import ImproperlyConfigured
@@ -139,3 +140,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DATABASES['default'] = dj_database_url.config(
+#    conn_max_age=600, ssl_require=True)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("User login page")
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def result(request):
+    full = request.GET['fulltext']
+    words = full.split()
+    words_dic = {}
+    for word in words:
+        if word in words_dic:
+            words_dic[word] += 1
+        else:
+            words_dic[word] = 1
+
+    return render(request, 'result.html',
+                  {'full': full, 'length': len(words), 'dic': words_dic.items()})
 
 # Create your views here.
