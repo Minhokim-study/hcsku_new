4b33bdcae838fa50fddd9b804299754366ba05a7
Minho Kim <xtabletx@gmail.com>
2020-06-08 20:36:38 -0500

    settings.py change to heroku getenv

--- hcsku/settings.py
+++ hcsku/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+"""
 secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
 
 with open(secret_file) as f:
@@ -30,15 +31,15 @@ with open(secret_file) as f:
 
 
 def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    # 비밀 변수를 가져오거나 명시적 예외를 반환한다.
     try:
         return secrets[setting]
     except KeyError:
         error_msg = "Set the {} environment variable".format(setting)
         raise ImproperlyConfigured(error_msg)
+"""
 
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
