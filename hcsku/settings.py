30a5074857f2b8f4a080b1cc70c5c895a840083f
Minho Kim <xtabletx@gmail.com>
2020-06-08 20:51:07 -0500

    heroku static fix 2

--- hcsku/settings.py
+++ hcsku/settings.py
@@ -137,12 +137,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # DATABASES['default'] = dj_database_url.config(
 #    conn_max_age=600, ssl_require=True)
 db_from_env = dj_database_url.config(conn_max_age=500)
